c6f1469d6103986213b7c4d2e3cef3f4b339637e
JofielG <leifoj4@icloud.com>
2023-04-08 00:23:59 -0500

    Started the implementation of the new UI for the app. Moved it out of the folder in order to be used in the newMain file

---
+++ newMain.py
@@ -0,0 +1,23 @@
+from PyQt5 import uic
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtPrintSupport import *
+
+import os
+import sys
+import uuid
+
+class NewMain(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(NewMain, self).__init__(*args, **kwargs)
+        uic.loadUi("mainScreen.ui", self)
+        self.show()
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    app.setApplicationName("Megasolid Idiom")
+
+    window = NewMain()
+    app.exec_()
